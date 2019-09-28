f2d8f13c6f540ffd97117d750b79f3d8fdcb3566
Ruslan Khamidullin <rusllonrails@gmail.com>
2019-09-28 19:16:17 +0300

    [CHALLENGE 17] Multiples of 3 or 5
    **BRIEF:**
    
    If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
    The sum of these multiples is 23.
    
    So, write code which returns the sum of all the multiples of 3 or 5 below the number passed in.
    
    **EXAMPLES:**
    
    ```
    Multiples.new(10).run
      => 23
    
    Multiples.new(20).run
      => 78
    
    Multiples.new(200).run
      => 9168
    ```

---
+++ spec/unit/17_multiple_of_three_and_five_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require './challenges/17_multiple_of_three_and_five'
+
+describe '17: Multiples of 3 or 5' do
+  it 'allows to get the sum of all the multiples of 3 or 5 below the number passed in' do
+    expect(
+      Multiples.new(10).run
+    ).to be(23)
+
+    expect(
+      Multiples.new(20).run
+    ).to be(78)
+
+    expect(
+      Multiples.new(200).run
+    ).to be(9168)
+  end
+end
